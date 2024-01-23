7c73da322e04eeeb041c974d5e5f98f22dc2ce99
kyj5599 <277k@naver.com>
2024-01-23 14:52:53 +0900

    const와 let의 차이

---
+++ app.js
@@ -0,0 +1,17 @@
+// const 값 변경 불가능
+// const a = 5; // Number
+// const b = 2; // Number
+// const myName = "yj"; // String
+
+// let 값 변경 가능
+const a = 5; // Number
+const b = 2; // Number
+let myName = "yj"; // String
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "yujin";
+console.log("your new name is " + myName);
