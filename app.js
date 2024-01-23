7cf3c3bc2aef2d8d33955beda1ccc8c6e6bf2206
kyj5599 <277k@naver.com>
2024-01-23 16:50:10 +0900

    객체 object

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// 데이터 구조
-// array 배열 []
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+// object
+const player = {
+  name: "yj",
+  points: 10,
+  fat: true,
+};
+console.log(player.name); // yj
+console.log(player["name"]); // yj
 
-// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-console.log(daysOfWeek); // ["mon", "tue", "wed", "thu", "fri", "sat"]
-
-// Get Item from Array
-console.log(daysOfWeek[4]); // fri
-
-// Add one more day to the array
-daysOfWeek.push("sun");
-console.log(daysOfWeek); // ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+console.log(player); // {name: "yj", points: 10, fat: true}
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player); // {name: "yj", points: 25, fat: true, lastName: "potato"}
