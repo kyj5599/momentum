d88baa4bd6cf092a88e6f552efe6f0bb0709cbc7
kyj5599 <277k@naver.com>
2024-01-24 16:50:08 +0900

    함수 function 2 calculate

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// function
-// 계속 반복해서 사용할 수 있는 코드 조각
-// argument(인자)를 넣어줘야함 => nameOfPerson, age
-function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// function 2
+// calculate
+// firstNumber, secondNumber은 plus함수 안에서만 존재한다.
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
 }
-// 함수를 실행시키려면
-// 함수명();
-sayHello("yj", 25);
-sayHello("dal", 10);
-sayHello("lynn", 32);
+function divide(a, b) {
+  console.log(a / b);
+}
+
+plus(60, 8);
+divide(98, 20);
