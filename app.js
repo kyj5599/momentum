bfe6b7c33194beba5f616810a232dde88cb2802f
kyj5599 <277k@naver.com>
2024-01-24 16:40:37 +0900

    함수 function 1

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// object
-const player = {
-  name: "yj",
-  points: 10,
-  fat: true,
-};
-console.log(player.name); // yj
-console.log(player["name"]); // yj
-
-console.log(player); // {name: "yj", points: 10, fat: true}
-player.lastName = "potato";
-player.points = player.points + 15;
-console.log(player); // {name: "yj", points: 25, fat: true, lastName: "potato"}
+// function
+// 계속 반복해서 사용할 수 있는 코드 조각
+// argument(인자)를 넣어줘야함 => nameOfPerson, age
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+// 함수를 실행시키려면
+// 함수명();
+sayHello("yj", 25);
+sayHello("dal", 10);
+sayHello("lynn", 32);
