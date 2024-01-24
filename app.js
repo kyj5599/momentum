ec01189a7bba38ddf0b75953c1a976618619034a
kyj5599 <277k@naver.com>
2024-01-24 20:57:22 +0900

    조건문3(음주가능 나이 계산)   if / else if / else  &&(AND) / ||(OR)

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// 조건문 (conditional) 2
-// NaN : Not a Number
-// isNaN() : 숫자가 아닌지 확인하는 함수
-// isNaN은 boolean을 반환(return)한다.
+// 조건문 (conditional) 3
 const age = parseInt(prompt("How old are you?"));
 
-// if
-if (isNaN(age)) {
-  // isNaN(age) === true
-  console.log("Please write a number");
-} else {
-  console.log("Thank you for writing your age.");
+// if / else if / else
+if (isNaN(age) || age < 0) {
+  console.log("Please a real write a number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  // && 는 AND => 둘다 ture 이어야 함
+  // || 는 OR => 둘중 하나만 true이면 됨
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want");
 }
