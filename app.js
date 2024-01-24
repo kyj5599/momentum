bf4de67abd8676bf210ef632584df10053e165a4
kyj5599 <277k@naver.com>
2024-01-24 19:13:37 +0900

    함수에서 return

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 // calculator
+// return
 const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   multiply: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(1, 2);
-calculator.minus(1, 2);
-calculator.divide(1, 2);
-calculator.multiply(1, 2);
-calculator.power(1, 2);
+const plusResult = calculator.add(2, 3); // 5
+const minusResult = calculator.minus(plusResult, 10); // -5
+const multiplyResult = calculator.multiply(10, minusResult); // -50
+const divideResult = calculator.divide(multiplyResult, plusResult); // -10
+const powerResult = calculator.power(divideResult, minusResult); // 1e+10
