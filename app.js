39d475cf599fbd074531c1000da06150b8324010
kyj5599 <277k@naver.com>
2024-01-24 20:25:42 +0900

    조건문2 isNaN()

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// 조건문 (conditional)
-// const age = prompt("How old are you?");
-// typeof : 변수의 타입을 알려줌
-// parseInt() : 문자열을 숫자로 변환
-// console.log(typeof "15", typeof parseInt("15")); // string number
-// console.log(age, parseInt(age)); // 15(string) 15(number) // 문자를 입력하면 NaN 출력
+// 조건문 (conditional) 2
 // NaN : Not a Number
-
+// isNaN() : 숫자가 아닌지 확인하는 함수
+// isNaN은 boolean을 반환(return)한다.
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
+// if
+if (isNaN(age)) {
+  // isNaN(age) === true
+  console.log("Please write a number");
+} else {
+  console.log("Thank you for writing your age.");
+}
