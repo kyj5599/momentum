3332f8cb2f1ef01401df7af8303f4087634967e1
kyj5599 <277k@naver.com>
2024-01-25 18:51:10 +0900

    Events_Click(JAVASCRIPT ON THE BROWSER)

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const hellos = document.getElementsByClassName("hello");
+const title = document.querySelector("div.hello:first-child h1");
 
-// querySelector : element를 CSS 방식으로 검색할 수 있음
-// 첫번째 element만 가져옴
-// 다 가져오고 싶으면 querySelectorAll
-const title = document.querySelector(".hello h1");
+function handleTitleClick() {
+  // console.log("title was clicked!");
+  title.style.color = "blue";
+}
 
-// getElementById : id로 element를 검색할 수 있음
-// const title = document.querySelector("#hello");
-// const title = document.getElementById("hello");
-
-console.log(title);
+title.addEventListener("click", handleTitleClick);
