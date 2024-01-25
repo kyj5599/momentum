52d6c6f254d308e11ab8cd03e5f565ffabec7a24
kyj5599 <277k@naver.com>
2024-01-25 22:16:29 +0900

    코드 챌린지1(EventHandler)

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+// 1. 마우스가 title위로 올라가면 텍스트가 변경되어야 합니다.
+// 2. 마우스가 title을 벗어나면 텍스트가 변경되어야 합니다.
+// 3. 브라우저 창의 사이즈가 변하면 title이 바뀌어야 합니다.
+// 4. 마우스를 우 클릭하면 title이 바뀌어야 합니다.
+// 5. title의 색상은 colors 배열에 있는 색을 사용해야 합니다.
+// 6. .css 와 .html 파일은 수정하지 마세요.
+// 7. 모든 함수 핸들러는 superEventHandler내부에 작성해야 합니다.
 
-console.dir(h1);
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
-function handleh1Click() {
-  h1.style.color = "blue";
-}
+const title = document.querySelector("h1");
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
+const superEventHandler = {
+  mouseOver: () => {
+    title.innerHTML = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+  mouseLeave: () => {
+    title.innerHTML = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+  resize: () => {
+    title.innerHTML = "You just resized!";
+    title.style.color = colors[2];
+  },
+  contextMenu: () => {
+    title.innerHTML = "That was a right click!";
+    title.style.color = colors[4];
+  },
+};
 
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("All good");
-}
-
-// h1.onClick = handleh1Click;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+title.addEventListener("mouseover", superEventHandler.mouseOver);
+title.addEventListener("mouseleave", superEventHandler.mouseLeave);
+window.addEventListener("resize", superEventHandler.resize);
+window.addEventListener("contextmenu", superEventHandler.contextMenu);
