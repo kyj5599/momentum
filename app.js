b10fada89d8325591af9c8692a885a34505b0515
kyj5599 <277k@naver.com>
2024-01-25 17:14:56 +0900

    HTML in Javascript

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-// 조건문 (conditional) 3
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-// if / else if / else
-if (isNaN(age) || age < 0) {
-  console.log("Please a real write a number");
-} else if (age < 18) {
-  console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-  // && 는 AND => 둘다 ture 이어야 함
-  // || 는 OR => 둘중 하나만 true이면 됨
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want");
-}
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
