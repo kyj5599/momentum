fdf978136726f43e18ea8a96a70e7ed3f7f77d15
kyj5599 <277k@naver.com>
2024-01-25 17:29:30 +0900

    Searching For Elements / querySelector : element를 CSS 방식으로 검색

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const title = document.getElementById("title");
+const hellos = document.getElementsByClassName("hello");
 
-title.innerText = "Got you!";
+// querySelector : element를 CSS 방식으로 검색할 수 있음
+// 첫번째 element만 가져옴
+// 다 가져오고 싶으면 querySelectorAll
+const title = document.querySelector(".hello h1");
 
-console.log(title.id);
-console.log(title.className);
+// getElementById : id로 element를 검색할 수 있음
+// const title = document.querySelector("#hello");
+// const title = document.getElementById("hello");
+
+console.log(title);
