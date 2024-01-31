d3148a84049001b31529003097fe75b72ae910cf
kyj5599 <277k@naver.com>
2024-01-31 13:01:05 +0900

    JS Casino

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+const maxNumber = document.getElementById("maxNumber");
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
-
-// preventDefault() : 기본 동작을 막는다.
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  // localStorage : 작은 정보를 유저 컴퓨터에 저장하는 방법
-  // setItem : localStorage에 정보를 저장하는 방법
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "" && maxNumber === "") {
+    return;
+  }
+  const max = maxNumber.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
 }
 
-// submit : form을 제출할 때 발생하는 이벤트 => 브라우저의 기본 동작을 막게 된다.
-// loginForm.addEventListener("submit", onLoginSubmit);
-
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-// getItem : localStorage에 있는 정보를 가져오는 방법
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-  // show the form
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
-} else {
-  // show the greeting
-  paintGreetings(savedUsername);
-}
+guessForm.addEventListener("submit", handleGuessSubmit);
