307b323fddec952cf4dbf1b8d6b44f87f0f6180d
kyj5599 <277k@naver.com>
2024-02-04 16:36:54 +0900

    TODOLIST_Loading To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,19 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
 // toDos : 할 일을 저장하는 배열 array
-const toDos = [];
+// const -> let : 값이 변할 수 있음
+let toDos = [];
+
 // 브라우저 저장 : localStorage => array, object를 저장할 수 없음 / text만 저장 가능
 // saveToDos() : toDos를 localStorage에 저장하는 함수
 function saveToDos() {
-  // ** 중요 ** JSON.stringify() : JavaScript 값이나 객체를 JSON 문자열로 변환
+  // ** 중요 ** JSON.stringify() : JavaScript 값이나 객체를 JSON 문자열로 변환, string으로 변환
   // ex) ["a","b","c"]
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 // deleteToDo() : 할 일을 삭제하는 함수
@@ -44,3 +49,15 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// localStorage에 저장된 값이 있으면 가져와서 화면에 출력
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (saveToDos !== null) {
+  // parsedToDos : JavaScript에서 사용가능한 object로 변환 => array로 변환
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  // forEach() : 배열의 각 요소에 대해 한 번씩 제공한 함수를 실행
+  // 위의 paintToDo 함수를 가져옴
+  parsedToDos.forEach(paintToDo);
+}
