f9b0e36da42dd6261f76bece90150a9ebaa04496
kyj5599 <277k@naver.com>
2024-02-04 18:59:31 +0900

    TODOLIST_Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -21,13 +21,17 @@ function deleteToDo(event) {
   // parentElement : 부모 요소를 반환 => li
   const li = event.target.parentElement;
   li.remove();
+  // 클릭한 li.id와 toDos.id가 같지 않은 것만 반환
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 // paintToDo() : handleSubmit()에서 받은 값을 ul에 추가
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "✅";
   // addEventListener() : 이벤트 리스너를 추가 => 다 한 일을 클릭해서 삭제
@@ -43,8 +47,13 @@ function handleToDoSubmit(event) {
   // newTodo : toDoInput에 입력된 값
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  // newTodoObj : toDos에 추가할 객체
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
